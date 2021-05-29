aafc28deb9cc2260455340c156ca32d7464d07ef
Drachenfalke12 <marco.pa@gmx.de>
2021-05-29 22:26:28 +0200

    init

---
+++ MinecraftModManager/MainForm.xeto.cs
@@ -0,0 +1,31 @@
+using Eto.Drawing;
+using Eto.Forms;
+using Eto.Serialization.Xaml;
+using System;
+using System.Collections.Generic;
+
+namespace MinecraftModManager
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            XamlReader.Load(this);
+        }
+
+        protected void HandleClickMe(object sender, EventArgs e)
+        {
+            MessageBox.Show("I was clicked!");
+        }
+
+        protected void HandleAbout(object sender, EventArgs e)
+        {
+            new AboutDialog().ShowDialog(this);
+        }
+
+        protected void HandleQuit(object sender, EventArgs e)
+        {
+            Application.Instance.Quit();
+        }
+    }
+}
